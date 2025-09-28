522308766aa2ac577201d05e7b8e29c2883e850d
Lamont Crook <lamont@adobe.com>
2025-09-28 10:50:17 -0600

    fix animation position

--- blocks/sketch-screen/sketch-screen.js
+++ blocks/sketch-screen/sketch-screen.js
@@ -1,24 +1,21 @@
 export default async function decorate(block) {
-  //rightDiv is animationDiv
   let animationDiv;
 
-  if(!block.querySelector('div > div:nth-child(2)')){
+  if (!block.querySelector('div > div:nth-child(2)')) {
     animationDiv = block.querySelector('div > div:nth-child(1)');
   } else {
     animationDiv = block.querySelector('div > div:nth-child(2)');
   }
-  
-  let caption = false;
-  animationDiv.querySelectorAll('p').forEach(p => {
+
+  animationDiv.querySelectorAll('p').forEach((p) => {
     if (p.textContent.trim() !== '' && p.classList.length === 0 && !p.querySelector('a')) {
       p.classList.add('caption');
-      caption = p;
     }
   });
- 
+
   const pics = animationDiv.querySelectorAll('picture');
   const placeholder = pics[pics.length - 1];
-  if(pics.length > 1) pics[0].parentElement.classList.add('header-image');
+  if (pics.length > 1) pics[0].parentElement.classList.add('header-image');
   const link = animationDiv.querySelector('a');
 
   if (placeholder) {
@@ -41,5 +38,7 @@ export default async function decorate(block) {
       video.play();
     });
   }
-  animationDiv.querySelectorAll('p').forEach(p => {if(p.textContent.trim() === '' && p.classList.length === 0) {p.remove();}});
+  animationDiv.querySelectorAll('p').forEach((p) => {
+    if (p.textContent.trim() === '' && p.classList.length === 0) p.remove();
+  });
 }
